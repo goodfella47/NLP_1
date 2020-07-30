35667b9fb01658cf963071341d19e89b3203d95a
goodfella47 <goodfella47@gmail.com>
2020-07-30 10:08:01 +0300

    Update generate_comp_tagged.py

--- generate_comp_tagged.py
+++ generate_comp_tagged.py
@@ -1,20 +1,23 @@
-from MEMM2 import MEMM
+from MEMM import MEMM
 import pickle
 
 memm = MEMM()
 
-weights_path = 'train1_final_expanded.pkl'
+weights_path = 'train1_weights.pkl'
 with open(weights_path, 'rb') as f:
     optimal_params = pickle.load(f)
 weights = optimal_params
 
 memm.predict_with_predefined_weights('train1.wtag', 'comp1.words', weights=weights,
-                                     prediction_path='comp_m1_305320400_4.wtag', beam_width=3)
+                                     prediction_path='comp_m1_305320400.wtag', beam_width=3)
 
-weights_path = 'train2_final_expanded.pkl'
+weights_path = 'train2_weights.pkl'
 with open(weights_path, 'rb') as f:
     optimal_params = pickle.load(f)
 weights = optimal_params
 
 memm.predict_with_predefined_weights('train2.wtag', 'comp2.words', weights=weights,
-                                     prediction_path='comp_m2_305320400_4.wtag', beam_width=3)
+                                     prediction_path='comp_m2_305320400.wtag', beam_width=3)
+
+
+
